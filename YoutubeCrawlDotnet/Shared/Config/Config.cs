f451631193ccf4217e1bfcde79c0cd247ab17d87
najongjine <najongjin3@hotmail.com>
2021-02-20 14:28:45 +0900

    streaming api fix

--- YoutubeCrawlDotnet/Shared/Config/Config.cs
+++ YoutubeCrawlDotnet/Shared/Config/Config.cs
@@ -11,8 +11,5 @@ namespace YoutubeCrawlDotnet.Shared.Config
     public static readonly string OtherVideoPhysicalFilePath = "H:/OtherCrawled";
     public static readonly string YoutubeApiKey = "YOUR API KEY";
     public static readonly String YoutubeAPIBaseURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1000";
-    
-    public static readonly string BaseURL = "https://localhost:44328";
-
   }
 }
